61d27e8b913df2ca92b0a9c5797c69ae3cae65c7
Khurshid7 <98460383+Khurshid7@users.noreply.github.com>
2024-01-11 00:20:07 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'save_screen.dart';
-import 'read_screen.dart';
+import 'main_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,10 +12,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SaveScreen(),
-      routes: {
-        '/read': (context) => ReadScreen(),
-      },
+      home: MainScreen(),
     );
   }
 }
+
